e2dc18ecb65130ef91a6ffb06b66d57bd5ac2431
Lawrence Job <lawrencejob@live.co.uk>
2020-10-06 23:35:31 +0100

    Step 4: Allow custom delimiters

--- InterviewTest/Services/StringSummer.cs
+++ InterviewTest/Services/StringSummer.cs
@@ -1,27 +1,59 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace InterviewTest.Services
 {
     public class StringSummer : IStringSummer
     {
+
         public int Add(string input) 
         {
 
+            var delimiters = new char[] { ',', '\n'};
+
+            // deal with the special case of an empty input
             if (input == "") {
                 return 0;
             }
 
-            var numbersAsStrings = input.Split(new char[] { ',', '\n'});
+            // if there is a delimiter-specifying header in the file...
+            if (input.StartsWith("//")) {
 
-            if (numbersAsStrings.Contains(""))
-            {
-                throw new ArgumentException("Invalid argument - number fields (spaces between delimiters) cannot be empty");
+                // extract the new delimiter
+                delimiters = new char[] { input[2] };
+
+                // cut the delimiter section from the front of the string
+                input = input.Substring(4);
             }
 
-            return numbersAsStrings
-                .Select(int.Parse)
-                .Sum();
+            var numbersAsStrings = ParseNumbers(delimiters, input);
+
+            return numbersAsStrings.Sum();
+        }
+
+        private IEnumerable<int> ParseNumbers(char[] delimiters, string input) {
+
+            // break up the string by the delimiters
+            foreach (string current in input.Split(delimiters)) {
+
+                System.Console.WriteLine($"Reading {current}");
+
+                // make sure there are no double-delimiters (leaving empty strings after the split) e.g. "1,\n"
+                if (string.IsNullOrWhiteSpace(current))
+                {
+                    throw new ArgumentException("Invalid argument - number fields (spaces between delimiters) cannot be empty");
+                }
+
+                // try to interpret the integer, and (yield) return if possible
+                if (int.TryParse(current, NumberStyles.None, null, out var convertedInt)) {
+                    yield return convertedInt;
+                }
+                else {
+                    throw new ArgumentException($"Could not convert {current} to a number.");
+                }
+            }
         }
     }
 }
