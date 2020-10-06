5b0e5dcb0b5a2560a550e2f4fbe5473b63322e8c
Lawrence Job <lawrencejob@live.co.uk>
2020-10-06 18:33:26 +0100

    Initialise .Net Core console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace creative_technologies_interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
