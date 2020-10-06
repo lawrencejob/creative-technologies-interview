7c8f72d08217a7cf03b618f3589470060ad2b13b
Lawrence Job <lawrencejob@live.co.uk>
2020-10-06 20:11:56 +0100

    Step 3: Compress tests and add support for new line delimiter

---
+++ InterviewTest.Tests/InterviewCriteria.cs
@@ -0,0 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using InterviewTest.Services;
+
+namespace InterviewTest.Tests
+{
+    [TestClass]
+    public class InterviewCriteria
+    {
+        [DataTestMethod]
+        [DataRow("", 0, DisplayName = "Empty string returns 0")]
+        [DataRow("123", 123, DisplayName = "Single number returns self")]
+        [DataRow("123,456", 579, DisplayName = "Two numbers add successfully")]
+        public void Step1_VerifyBasicStrings(string input, int expected)
+        {
+            var summer = new StringSummer();
+            var output = summer.Add(input);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [DataTestMethod]
+        [DataRow("1,2,3,4,5,6,7,8,9", 45)]
+        [DataRow("123,1354,34534,4654,1,2,3", 40671)]
+        public void Step2_AnyNumberOfNumbers(string input, int expected)
+        {
+            var summer = new StringSummer();
+            var output = summer.Add(input);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [DataTestMethod]
+        [DataRow("1,2,3\n4,5,6,7,8,9", 45)]
+        [DataRow("123,1354,34534,4654,1,2\n3", 40671)]
+        public void Step3_AllowNewLinesAndCommas(string input, int expected)
+        {
+            var summer = new StringSummer();
+            var output = summer.Add(input);
+
+            Assert.AreEqual(expected, output);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Step3_DisallowDoubleDelimiter()
+        {
+            var summer = new StringSummer();
+            var output = summer.Add("1,\n");
+        }
+    }
+}
